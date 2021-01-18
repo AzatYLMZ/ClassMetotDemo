d9d4bbb79da1242283bf54dbf2c2f27198a0604b
Azat Yılmaz <Azat.Ylmz@hotmail.com>
2021-01-18 03:36:05 +0300

    Ödev 3

---
+++ ClassMetotDemo/MusteriManeger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+//MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme, silme gibi metotları simule ediniz.
+
+namespace ClassMetotDemo
+{
+    class MusteriManeger
+    {
+        List<Musteri> Musteriler = new List<Musteri>();
+        public void Ekle(Musteri musteri)
+        {
+            Musteriler.Add(musteri);
+            Console.WriteLine("Eklenen Müşteri : " + musteri.Id + " " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        
+
+        public void Listele()
+        {
+            Console.WriteLine("Müşteriler");
+            foreach (var item in Musteriler)
+            {
+                Console.WriteLine("-----------");
+                Console.WriteLine("Müşteri Id :" + item.Id);
+                Console.WriteLine("Müşteri Adı :" + item.Ad);
+                Console.WriteLine("Müşteri Soyadı :" + item.Soyad);
+            }
+        }
+        
+        public void Sil(Musteri musteri) 
+        {
+            Musteriler.Remove(musteri);
+            Console.WriteLine("Silinen Müşteri : " + musteri.Id+ " " + musteri.Ad+ " " + musteri.Soyad);
+        }
+
+        
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
+//Ödev3
+//Şimdi Github hesabınıza biraz hit alalım :)
+
+//ClassMetotDemo isimli bir proje oluşturunuz.
+//Bunu Github'a aktarınız.
+//Projeniz şunu yapacak.
+
+//Bir bankada müşteri takibi yapmak istiyorsunuz.
+//Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+//MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme, silme gibi metotları simule ediniz.
+
 namespace ClassMetotDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Azat";
+            musteri1.Soyad = "Yılmaz";
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Walter";
+            musteri2.Soyad = "White";
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Jessy";
+            musteri3.Soyad = "Pinkman";
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 4;
+            musteri4.Ad = "Saul";
+            musteri4.Soyad = "Goodman";
+
+            MusteriManeger musteriManeger = new MusteriManeger();
+
+            //Müşteri Ekleme Metodu
+
+            musteriManeger.Ekle(musteri1);
+            musteriManeger.Ekle(musteri2);
+            musteriManeger.Ekle(musteri3);
+            musteriManeger.Ekle(musteri4);
+
+            //Müşteri silme Metodu
+
+            musteriManeger.Sil(musteri4);
+
+            musteriManeger.Listele();
+
         }
+        
     }
 }
